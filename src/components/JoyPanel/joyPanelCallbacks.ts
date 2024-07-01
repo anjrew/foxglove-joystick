ce1fa372a123fecb8bb7984878d7ad5a72193cbd
Andrew Johnson <andrewmjohnson549@gmail.com>
2024-07-01 23:23:56 +0200

    Fix Joypad mapping

--- src/components/JoyPanel/joyPanelCallbacks.ts
+++ src/components/JoyPanel/joyPanelCallbacks.ts
@@ -1,124 +1,123 @@
-import React, { useCallback } from 'react';
 import { fromDate } from "@foxglove/rostime";
-import { Joy, KbMap } from '../../types';
-import { PanelConfig } from '../../config';
+import React, { useCallback } from "react";
+
+import { PanelConfig } from "../../config";
+import { Joy, KbMap } from "../../types";
+import { getGameToJoyTransform } from "../../utils/gamepadMappings";
 
 export function useJoyPanelCallbacks(
   config: PanelConfig,
   setConfig: React.Dispatch<React.SetStateAction<PanelConfig>>,
   setJoy: React.Dispatch<React.SetStateAction<Joy | undefined>>,
   setTrackedKeys: React.Dispatch<React.SetStateAction<Map<string, KbMap> | undefined>>,
-  setKbEnabled: React.Dispatch<React.SetStateAction<boolean>>
+  setKbEnabled: React.Dispatch<React.SetStateAction<boolean>>,
 ) {
-  const handleKeyDown = useCallback((event: KeyboardEvent) => {
-    setTrackedKeys((oldTrackedKeys) => {
-      if (oldTrackedKeys?.has(event.key)) {
-        const newKeys = new Map(oldTrackedKeys);
-        const k = newKeys.get(event.key);
-        if (k !== undefined) {
-          k.value = 1;
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      setTrackedKeys((oldTrackedKeys) => {
+        if (oldTrackedKeys?.has(event.key)) {
+          const newKeys = new Map(oldTrackedKeys);
+          const k = newKeys.get(event.key);
+          if (k !== undefined) {
+            k.value = 1;
+          }
+          return newKeys;
         }
-        return newKeys;
-      }
-      return oldTrackedKeys;
-    });
-  }, [setTrackedKeys]);
+        return oldTrackedKeys;
+      });
+    },
+    [setTrackedKeys],
+  );
 
-  const handleKeyUp = useCallback((event: KeyboardEvent) => {
-    setTrackedKeys((oldTrackedKeys) => {
-      if (oldTrackedKeys) {
-        const newKeys = new Map(oldTrackedKeys);
-        const k = newKeys.get(event.key);
-        if (k) {
-          k.value = 0;
+  const handleKeyUp = useCallback(
+    (event: KeyboardEvent) => {
+      setTrackedKeys((oldTrackedKeys) => {
+        if (oldTrackedKeys) {
+          const newKeys = new Map(oldTrackedKeys);
+          const k = newKeys.get(event.key);
+          if (k) {
+            k.value = 0;
+          }
+          return newKeys;
         }
-        return newKeys;
-      }
-      return oldTrackedKeys;
-    });
-  }, [setTrackedKeys]);
+        return oldTrackedKeys;
+      });
+    },
+    [setTrackedKeys],
+  );
 
-  const interactiveCb = useCallback((interactiveJoy: Joy) => {
-    if (config.dataSource !== "interactive") return;
-    const tmpJoy = {
-      header: {
-        frame_id: config.publishFrameId,
-        stamp: fromDate(new Date()),
-      },
-      axes: interactiveJoy.axes,
-      buttons: interactiveJoy.buttons,
-    } as Joy;
-    setJoy(tmpJoy);
-  }, [config.dataSource, config.publishFrameId, setJoy]);
-
-  const handleKbSwitch = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setKbEnabled(event.target.checked);
-  }, [setKbEnabled]);
-
-  const handleGamepadConnect = useCallback((gp: Gamepad) => {
-    setConfig((prevConfig: PanelConfig) => ({
-      ...prevConfig,
-      options: {
-        ...prevConfig.options,
-        availableControllers: [...prevConfig.options.availableControllers, gp],
-      },
-      dataSource: prevConfig.dataSource,
-      subJoyTopic: prevConfig.subJoyTopic,
-      gamepadId: prevConfig.gamepadId,
-      publishMode: prevConfig.publishMode,
-      // Add other missing properties here
-    }));
-    console.log(`Gamepad ${gp.index} connected!`);
-  }, [setConfig]);
-
-  const handleGamepadDisconnect = useCallback((gp: Gamepad) => {
-    setConfig((prevConfig: PanelConfig) => {
-      const newOptions = {
-        ...prevConfig.options,
-        availableControllers: prevConfig.options.availableControllers.filter(
-          (c) => c.id !== gp.id
-        ),
-      };
-      return {
-        ...prevConfig,
-        options: newOptions,
-      };
-    });
-    console.log(`Gamepad ${gp.index} disconnected!`);
-  }, [setConfig]);
-
-  const handleGamepadUpdate = useCallback((gp: Gamepad) => {
-      if (config.dataSource !== "gamepad" || config.gamepadId !== gp.index) {
+  const interactiveCb = useCallback(
+    (interactiveJoy: Joy) => {
+      if (config.dataSource !== "interactive") {
         return;
       }
-  
-      console.log(`Gamepad ${gp.index} ${config.layoutName} updating!`);
       const tmpJoy = {
         header: {
           frame_id: config.publishFrameId,
           stamp: fromDate(new Date()),
         },
-        axes: gp.axes.map((axis) => -axis),
-        buttons: gp.buttons.map((button) => (button.pressed ? 1 : 0)),
+        axes: interactiveJoy.axes,
+        buttons: interactiveJoy.buttons,
       } as Joy;
-  
-      if (config.layoutName === "xbox") {
-        const triggerLeftAxis: number = gp.buttons[6]?.value ?? 0;
-        const triggerRightAxis: number = gp.buttons[7]?.value ?? 0;
-        tmpJoy.axes = [...tmpJoy.axes, triggerLeftAxis, triggerRightAxis];
-  
-        const xboxButtons = gp.buttons.map((button, index) => {
-          if (index === 6 || index === 7) {
-            return button.value;
-          } else {
-            return button.pressed ? 1 : 0;
-          }
-        });
-        tmpJoy.buttons = xboxButtons;
-      }
-  
       setJoy(tmpJoy);
-    }, [config.dataSource, config.gamepadId, config.layoutName, config.publishFrameId, setJoy]);
+    },
+    [config.dataSource, config.publishFrameId, setJoy],
+  );
+
+  const handleKbSwitch = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setKbEnabled(event.target.checked);
+    },
+    [setKbEnabled],
+  );
+
+  const handleGamepadConnect = useCallback(
+    (gp: Gamepad) => {
+      setConfig((prevConfig: PanelConfig) => ({
+        ...prevConfig,
+        options: {
+          ...prevConfig.options,
+          availableControllers: [...prevConfig.options.availableControllers, gp],
+        },
+        dataSource: prevConfig.dataSource,
+        subJoyTopic: prevConfig.subJoyTopic,
+        gamepadId: prevConfig.gamepadId,
+        publishMode: prevConfig.publishMode,
+        // Add other missing properties here
+      }));
+      console.log(`Gamepad ${gp.index} connected!`);
+    },
+    [setConfig],
+  );
+
+  const handleGamepadDisconnect = useCallback(
+    (gp: Gamepad) => {
+      setConfig((prevConfig: PanelConfig) => {
+        const newOptions = {
+          ...prevConfig.options,
+          availableControllers: prevConfig.options.availableControllers.filter(
+            (c) => c.id !== gp.id,
+          ),
+        };
+        return {
+          ...prevConfig,
+          options: newOptions,
+        };
+      });
+      console.log(`Gamepad ${gp.index} disconnected!`);
+    },
+    [setConfig],
+  );
+
+  const handleGamepadUpdate = useCallback(
+    (gp: Gamepad) => {
+      if (config.dataSource !== "gamepad" || config.gamepadId !== gp.index) {
+        return;
+      }
+      setJoy(getGameToJoyTransform(config.publishFrameId, config.layoutName, gp));
+    },
+    [config.dataSource, config.gamepadId, config.layoutName, config.publishFrameId, setJoy],
+  );
 
   return {
     handleKeyDown,
--- src/utils/gamepadMappings.ts
+++ src/utils/gamepadMappings.ts
@@ -1,51 +1,102 @@
+import { fromDate } from "@foxglove/rostime";
 import _ from "lodash";
 
 import cheapo from "../mappings/cheapo.json";
 import ipega9083s from "../mappings/ipega-9083s.json";
 import steamdeck from "../mappings/steamdeck.json";
 import xbox from "../mappings/xbox.json";
-import { DisplayMapping, Joy } from "../types";
+import { DisplayMapping, Header, Joy } from "../types";
 
-const gamepadMappings = {
+// Define types for the mapping functions
+type GetMappingFunction = () => DisplayMapping;
+type JoyTransformFunction = (joy: Joy) => Joy;
+type GameToJoyTransformFunction = (publishFrameId: string, gp: Gamepad) => Joy;
+
+// Define an interface for each gamepad mapping entry
+interface GamepadMappingEntry {
+  label: string;
+  getMapping: GetMappingFunction;
+  getJoyTransform: JoyTransformFunction;
+  getGameToJoyTransform: GameToJoyTransformFunction;
+}
+
+export type GamepadMappingKey = "steamdeck" | "ipega-9083s" | "xbox" | "xbox2" | "cheapo";
+
+// Define the type for the entire gamepadMappings object
+type GamepadMappings = {
+  [key: string]: GamepadMappingEntry;
+};
+
+const defaultGameToJoyTransform = (publishFrameId: string, gp: Gamepad): Joy => {
+  return {
+    header: {
+      frame_id: publishFrameId,
+      stamp: fromDate(new Date()),
+    } as Header,
+    axes: gp.axes.map((axis) => -axis),
+    buttons: gp.buttons.map((button) => (button.pressed ? 1 : 0)),
+  } as Joy;
+};
+
+const gamepadMappings: GamepadMappings = {
   steamdeck: {
     label: "Steam Deck",
     getMapping: () => steamdeck,
     getJoyTransform: (joy: Joy) => joy,
+    getGameToJoyTransform: defaultGameToJoyTransform,
   },
   "ipega-9083s": {
     label: "iPega PG-9083s",
     getMapping: () => ipega9083s,
     getJoyTransform: (joy: Joy) => joy,
+    getGameToJoyTransform: defaultGameToJoyTransform,
   },
   xbox: {
     label: "Xbox",
     getMapping: () => xbox,
     getJoyTransform: (joy: Joy) => joy,
+    getGameToJoyTransform: xboxPadToJoyTransform,
   },
   xbox2: {
     label: "Xbox Axis Reverse",
     getMapping: () => xbox,
     getJoyTransform: (joy: Joy) => {
-      const newJoy = _.cloneDeep(joy);
-      newJoy.axes = newJoy.axes.map((axis) => -axis);
-      return newJoy;
+      joy.axes = joy.axes.map((axis) => -axis);
+      return joy;
     },
+    getGameToJoyTransform: xboxPadToJoyTransform,
   },
   cheapo: {
     label: "Cheap Controller",
     getMapping: () => cheapo,
     getJoyTransform: (joy: Joy) => joy,
+    getGameToJoyTransform: defaultGameToJoyTransform,
   },
 } as const;
 
-export type GamepadMappingKey = keyof typeof gamepadMappings;
+function xboxPadToJoyTransform(publishFrameId: string, gp: Gamepad): Joy {
+  const tmpJoy = defaultGameToJoyTransform(publishFrameId, gp);
+  const triggerLeftAxis: number = gp.buttons[6]?.value ?? 0;
+  const triggerRightAxis: number = gp.buttons[7]?.value ?? 0;
+  tmpJoy.axes = [...tmpJoy.axes, triggerLeftAxis, triggerRightAxis];
+
+  const xboxButtons = gp.buttons.map((button, index) => {
+    if (index === 6 || index === 7) {
+      return button.value;
+    } else {
+      return button.pressed ? 1 : 0;
+    }
+  });
+  tmpJoy.buttons = xboxButtons;
+  return tmpJoy;
+}
 
 export function getGamepadMapping(layoutName: GamepadMappingKey): DisplayMapping {
-  return gamepadMappings[layoutName].getMapping();
+  return gamepadMappings[layoutName]!.getMapping();
 }
 
 export function transformJoy(layoutName: GamepadMappingKey, joy: Joy): Joy {
-  return gamepadMappings[layoutName].getJoyTransform(joy);
+  return gamepadMappings[layoutName]!.getJoyTransform(joy);
 }
 
 export function getGamepadOptions(): { label: string; value: GamepadMappingKey }[] {
@@ -53,4 +104,12 @@ export function getGamepadOptions(): { label: string; value: GamepadMappingKey }
     label,
     value: key as GamepadMappingKey,
   }));
+}
+
+export function getGameToJoyTransform(
+  publishFrameId: string,
+  layoutName: GamepadMappingKey,
+  gp: Gamepad,
+): Joy {
+  return gamepadMappings[layoutName]!.getGameToJoyTransform(publishFrameId, gp);
 }
